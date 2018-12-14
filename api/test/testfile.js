e538c572ca1df0881d8f9bf8d346112c3316a19c
Anisha Jain <anishajain@outlook.com>
2018-12-14 02:30:02 -0800

    api project

---
+++ api/test/testfile.js
@@ -0,0 +1,21 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("site", () => {
+  it("Should have home page", done => {
+    // test that the home page loads
+    chai
+      .request("localhost:3000")
+      .get("/")
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.status.should.be.equal(200);
+        return done(); // Call done if the test completed successfully.
+      });
+  });
+});
